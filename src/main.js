1f8878df99584b726df185b885391c5a075349de
anovokmet <ante.novokmet@gdi.net>
2018-09-02 15:15:39 +0200

    fixed task switching, improved structure, added virtualization to row headers

--- src/main.js
+++ src/main.js
@@ -1,8 +1,33 @@
-import App from './App.html';
+import SvelteGantt from './Grid.html';
+import moment from "../node_modules/moment/src/moment.js";
+let startOfToday = moment().startOf('day')
 
-var app = new App({
-	target: document.body,
-	data: {}
-});
+let data = {
+	rows: [],
+	dependencies: []
+}
+
+for(let i = 0; i < 500; i++){
+	data.rows.push({
+		id: i,
+		label: 'Row #'+i,
+		tasks: [
+			{
+				id: i,
+				label: 'Task #'+i,
+				from: startOfToday.clone().set({'hour': 9, 'minute': 0}),
+				to: startOfToday.clone().set({'hour': 12, 'minute': 0})
+			}
+		]
+	});
+}
+data.dependencies.push({fromTask: data.rows[0].tasks[0], toTask: data.rows[1].tasks[0] });
+
+let options = {
+	from: startOfToday,
+	to: moment().endOf('day')
+}
+
+var app = SvelteGantt.create(document.body, data, options);
 
 export default app;
